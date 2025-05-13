dca23d73e70a0f3ab2a6ae938caae2457cf76d8d
ControlShiftEscape <maytarosheejsh@outlook.com>
2025-05-13 22:57:58 +0300

    Predicting content errors

--- barkanov.nikita/T3/main.cpp
+++ barkanov.nikita/T3/main.cpp
@@ -14,12 +14,12 @@
 
 int main(int argc, char* argv[]) {
 
-    //if (argc != 2) {
-    //    std::cerr << "ERROR: Exactly one argument must be passed.";
-    //    return 1;
-    //}
+    if (argc != 2) {
+        std::cerr << "ERROR: Exactly one argument must be passed.";
+        return 1;
+    }
     std::string filename;
-    //filename = argv[1];
+    filename = argv[1];
 
     std::string line;
     GeometryHandler cmdHandler;
